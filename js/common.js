89da7250162021dcd6866a96c49604aa998ad89c
Ruslan Chabin <pycc_lan@mail.ru>
2019-11-16 14:18:27 +0300

    BG video

--- js/common.js
+++ js/common.js
@@ -2,13 +2,42 @@ $(document).ready(function(){
 
 	var userFeed = new Instafeed({
 	  get: 'user',
-	  userId: '6792532565',
+	  userId: '3095949657',
 	  limit: '12',
 	  resolution: 'standard_resolution',
-	  template: '<div class="col-3 mb-3"><a href="{{image}}" title="{{caption}} target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid rounded-lg border border-secondary"/></a></div>',
-	  accessToken: '6792532565.1677ed0.92eab687a05e457c8a511d76e7470d73'
+	  template: '<div class="instaitem col-3 m-4 mb-5"><a href="{{image}}" title="{{caption}} target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+	  accessToken: '3095949657.1677ed0.745c759d24894ab782c120c3bd1367a9'
 	});
-
+	
  	userFeed.run();
-
+	
+	var btn = $('#button');
+	
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+	
+	$(function(){
+			$('#button').click(function(){
+				$('html, body').animate({scrollTop: 0}, 600);
+				return false;
+			});
+		});
+	$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+	});
 });
+
+//video
+
+var player = document.getElementById("video");
+player.playbackRate = 0.4;
+
