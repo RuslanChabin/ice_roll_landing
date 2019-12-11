e8664ccfa442c3e01b213e0df3d697d68733ae11
Ruslan Chabin <pycc_lan@mail.ru>
2019-12-11 06:43:50 +0300

    slider and sections

--- js/common.js
+++ js/common.js
@@ -39,5 +39,116 @@ $(document).ready(function(){
 //video
 
 var player = document.getElementById("video");
-player.playbackRate = 0.4;
+player.playbackRate = 0.8;
+
+//carusel
+
+function slideOne(){
+	$(".photo-slider-list div:nth-child(3)").removeClass().addClass("selected");
+	$(".photo-slider-list div:last-child").removeClass().addClass("nextRightSecond");
+	$(".photo-slider-list div:nth-child(4)").removeClass().addClass("next");
+	$(".photo-slider-list div:nth-child(2)").removeClass().addClass("prev");
+	$(".photo-slider-list div:first-child").removeClass().addClass("prevLeftSecond");
+	$(".slider-button").removeClass("slider-button--active");
+	$(".slider-button-1").addClass("slider-button--active");
+}
+
+function slideTwo(){
+	$(".photo-slider-list div:nth-child(4)").removeClass().addClass("selected");
+	$(".photo-slider-list div:last-child").removeClass().addClass("next");
+	$(".photo-slider-list div:nth-child(3)").removeClass().addClass("prev");
+	$(".photo-slider-list div:nth-child(2)").removeClass().addClass("prevLeftSecond");
+	$(".photo-slider-list div:first-child").removeClass().addClass("nextRightSecond");
+	$(".slider-button").removeClass("slider-button--active");
+	$(".slider-button-2").addClass("slider-button--active");
+}
+
+function slideThree(){
+	$(".photo-slider-list div:last-child").removeClass().addClass("selected");
+	$(".photo-slider-list div:nth-child(4)").removeClass().addClass("prev");
+	$(".photo-slider-list div:nth-child(3)").removeClass().addClass("prevLeftSecond");
+	$(".photo-slider-list div:nth-child(2)").removeClass().addClass("nextRightSecond");
+	$(".photo-slider-list div:first-child").removeClass().addClass("next");
+	$(".slider-button").removeClass("slider-button--active");
+	$(".slider-button-3").addClass("slider-button--active");
+}
+
+function slideFour(){
+	$(".photo-slider-list div:first-child").removeClass().addClass("selected");
+	$(".photo-slider-list div:last-child").removeClass().addClass("prev");
+	$(".photo-slider-list div:nth-child(4)").removeClass().addClass("prevLeftSecond");
+	$(".photo-slider-list div:nth-child(3)").removeClass().addClass("nextRightSecond");
+	$(".photo-slider-list div:nth-child(2)").removeClass().addClass("next");
+	$(".slider-button").removeClass("slider-button--active");
+	$(".slider-button-4").addClass("slider-button--active");
+};
+
+function slideFive(){
+	$(".photo-slider-list div:nth-child(2)").removeClass().addClass("selected");
+	$(".photo-slider-list div:last-child").removeClass().addClass("prevLeftSecond");
+	$(".photo-slider-list div:nth-child(4)").removeClass().addClass("nextRightSecond");
+	$(".photo-slider-list div:nth-child(3)").removeClass().addClass("next");
+	$(".photo-slider-list div:first-child").removeClass().addClass("prev");
+	$(".slider-button").removeClass("slider-button--active");
+	$(".slider-button-5").addClass("slider-button--active");
+}
+
+$(".photo-slider-list div:nth-child(3)").click(slideOne);
+$(".photo-slider-list div:nth-child(4)").click(slideTwo);
+$(".photo-slider-list div:last-child").click(slideThree);
+$('.photo-slider-list div:first-child').click(slideFour);
+$(".photo-slider-list div:nth-child(2)").click(slideFive);
+$(".slider-button-1").click(function(){
+	$(".slider-button").removeClass("slider-button--active");
+	$(this).addClass("slider-button--active");
+	$(document).ready(slideOne);
+});
+$(".slider-button-2").click(function(){
+	$(".slider-button").removeClass("slider-button--active");
+	$(this).addClass("slider-button--active");
+	$(document).ready(slideTwo);
+});
+$(".slider-button-3").click(function(){
+	$(".slider-button").removeClass("slider-button--active");
+	$(this).addClass("slider-button--active");
+	$(document).ready(slideThree);
+});
+$(".slider-button-4").click(function(){
+	$(".slider-button").removeClass("slider-button--active");
+	$(this).addClass("slider-button--active");
+	$(document).ready(slideFour);
+});
+$(".slider-button-5").click(function(){
+	$(".slider-button").removeClass("slider-button--active");
+	$(this).addClass("slider-button--active");
+	$(document).ready(slideFive);
+});
+
+$(".slider-arrow--right").click(function(){
+	if($(".photo-slider-list div:nth-child(3)").hasClass("selected")){
+		$(document).ready(slideTwo);
+	}else if($(".photo-slider-list div:nth-child(4)").hasClass("selected")){
+		$(document).ready(slideThree);
+	}else if($(".photo-slider-list div:last-child").hasClass("selected")){
+		$(document).ready(slideFour);
+	}else if($(".photo-slider-list div:first-child").hasClass("selected")){
+		$(document).ready(slideFive);
+	}else if($(".photo-slider-list div:nth-child(2)").hasClass("selected")){
+		$(document).ready(slideOne);
+	};
+});
+
+$(".slider-arrow--left").click(function(){
+	if($(".photo-slider-list div:nth-child(3)").hasClass("selected")){
+		$(document).ready(slideFive);
+	}else if($(".photo-slider-list div:nth-child(4)").hasClass("selected")){
+		$(document).ready(slideOne);
+	}else if($(".photo-slider-list div:last-child").hasClass("selected")){
+		$(document).ready(slideTwo);
+	}else if($(".photo-slider-list div:first-child").hasClass("selected")){
+		$(document).ready(slideThree);
+	}else if($(".photo-slider-list div:nth-child(2)").hasClass("selected")){
+		$(document).ready(slideFour);
+	};
+});
 
